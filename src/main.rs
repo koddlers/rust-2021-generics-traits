7dfddffc996b4d71b3018383e2b3a03d05b48f2f
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-06-27 19:30:00 +0600

    Add Module 03 - Defining Shared Behaviour with Traits > 03 - Default Implementations

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,18 @@ mod module_03;
 
 use module_02::using_generics_to_reduce_code_duplication;
 use module_02::using_generics_to_reduce_code_duplication_v2;
+use module_03::defining_shared_behaviour_with_traits;
+use module_03::defining_shared_behaviour_with_traits_v2;
+
 
 fn main() {
+    // Module 02 - Using Generics to Reduce Code Duplication
     // using_generics_to_reduce_code_duplication::why_do_we_need_generics();
     // using_generics_to_reduce_code_duplication::structs();
     // using_generics_to_reduce_code_duplication_v2::methods();
-    using_generics_to_reduce_code_duplication_v2::enums();
+    // using_generics_to_reduce_code_duplication_v2::enums();
+
+    // Module 03 - Defining Shared Behaviour with Traits
+    defining_shared_behaviour_with_traits::defining_a_trait();
+    defining_shared_behaviour_with_traits_v2::default_implementations();
 }
--- src/module_03.rs
+++ src/module_03.rs
@@ -63,6 +63,70 @@ pub mod defining_shared_behaviour_with_traits {
         println!("{} says {}", dog.name, dog.talk());
         println!("{} says {}", cat.name, cat.talk());
         println!("{} says {}", jelly.name, jelly.talk());
-        println!("{} says {}", butterfly.name, butterfly.talk());
+        println!("{} says {}\n", butterfly.name, butterfly.talk());
+    }
+}
+
+pub mod defining_shared_behaviour_with_traits_v2 {
+    struct Dog {
+        name: String,
+    }
+
+    struct Cat {
+        name: String,
+    }
+
+    struct Jellyfish {
+        name: String,
+    }
+
+    struct Butterfly {
+        name: String,
+    }
+
+    trait Animal {
+        fn talk(&self) -> String {
+            "I can't talk".to_string()
+        }
+    }
+
+    impl Animal for Dog {
+        fn talk(&self) -> String {
+            "Aaawooooo".to_string()
+        }
+    }
+
+    impl Animal for Cat {
+        fn talk(&self) -> String {
+            "Meow".to_string()
+        }
+    }
+
+    impl Animal for Jellyfish {}
+
+    impl Animal for Butterfly {}
+
+    pub fn default_implementations() {
+        let dog = Dog {
+            name: "Cujo".to_string()
+        };
+
+        let cat = Cat {
+            name: "Coco".to_string()
+        };
+
+        let jelly = Jellyfish {
+            name: "Jelly".to_string()
+        };
+
+        let butterfly = Butterfly {
+            name: "Monarch".to_string()
+        };
+
+        println!("With default implementation:");
+        println!("{} says {}", dog.name, dog.talk());
+        println!("{} says {}", cat.name, cat.talk());
+        println!("{} says {}", jelly.name, jelly.talk());
+        println!("{} says {}\n", butterfly.name, butterfly.talk());
     }
 }
